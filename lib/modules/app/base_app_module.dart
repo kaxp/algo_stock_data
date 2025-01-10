cb023691d88fdf63e192c6c715fec4fb98dc64f7
kaxp <kapilsahu232@gmail.com>
2025-01-10 13:07:25 +0530

    Add base app module class

---
+++ lib/modules/app/base_app_module.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+/// [BaseAppModule] is the primary module that will hold all the common
+/// dependencies
+class BaseAppModule extends Module {}
+
+class BaseAppModuleRoutes {
+  static const String homePage = '/';
+}
