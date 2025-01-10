6ba35bb11ddd7e42659c3db716c9b9ccd0318406
kaxp <kapilsahu232@gmail.com>
2025-01-10 16:18:12 +0530

    Add networking constants

---
+++ lib/networking/constants/network_constants.dart
@@ -0,0 +1,11 @@
+import 'package:algo_test/config/flavor_config.dart';
+
+class NetworkConstants {
+  static String prodBaseUrl = 'https://prices.algotest.xyz';
+
+  static String stagingBaseUrl = 'https://prices.algotest.xyz';
+
+  static String devBaseUrl = 'https://prices.algotest.xyz';
+
+  static final String baseUrl = FlavorConfig.instance!.values.baseUrl;
+}
