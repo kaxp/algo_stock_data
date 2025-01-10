aa9f64f6b46bcb5d6a9c940df05a1f85387f0868
kaxp <kapilsahu232@gmail.com>
2025-01-10 19:24:20 +0530

    Bind AppDio to BaseAppModule

--- lib/modules/app/base_app_module.dart
+++ lib/modules/app/base_app_module.dart
@@ -1,8 +1,22 @@
+import 'package:algo_test/networking/http_client.dart';
+import 'package:algo_test/networking/models/app_dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 /// [BaseAppModule] is the primary module that will hold all the common
 /// dependencies
-class BaseAppModule extends Module {}
+class BaseAppModule extends Module {
+  @override
+  List<Bind> get binds => [
+        Bind<AppDio>(
+          (i) => AppDio(
+            noAuthDio: httpClient(),
+          ),
+        ),
+      ];
+
+  @override
+  List<ModularRoute> get routes => [];
+}
 
 class BaseAppModuleRoutes {
   static const String homePage = '/';
