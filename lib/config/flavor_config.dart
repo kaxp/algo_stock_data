10d237b281db0b5f2f8369196c7b26d6cb2eef62
kaxp <kapilsahu232@gmail.com>
2025-01-10 13:02:23 +0530

    Add FlavorConfig class

---
+++ lib/config/flavor_config.dart
@@ -0,0 +1,33 @@
+enum Flavor { staging, production, development }
+
+class FlavorValues {
+  const FlavorValues({
+    required this.baseUrl,
+    required this.clientId,
+  });
+  final String baseUrl;
+  final String clientId;
+}
+
+class FlavorConfig {
+  factory FlavorConfig({required Flavor flavor, required FlavorValues values}) {
+    _instance ??= FlavorConfig._internal(
+        flavor, flavor.toString().split('.').last, values);
+    return _instance!;
+  }
+
+  FlavorConfig._internal(this.flavor, this.name, this.values);
+
+  final Flavor flavor;
+  final String name;
+  final FlavorValues values;
+  static FlavorConfig? _instance;
+
+  static FlavorConfig? get instance => _instance;
+
+  static bool isProduction() => _instance?.flavor == Flavor.production;
+
+  static bool isStaging() => _instance?.flavor == Flavor.staging;
+
+  static bool isDevelopment() => _instance?.flavor == Flavor.development;
+}
