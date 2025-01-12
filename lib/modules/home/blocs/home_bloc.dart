7a37521a70dfa1af651d1c8c48d0ebc5a03d2fd6
kaxp <kapilsahu232@gmail.com>
2025-01-12 22:12:22 +0530

    Integrate option chain api in bloc

--- lib/modules/home/blocs/home_bloc.dart
+++ lib/modules/home/blocs/home_bloc.dart
@@ -1,8 +1,37 @@
+import 'package:algo_test/modules/home/models/option_chain_response.dart';
+import 'package:algo_test/modules/home/repositories/home_repo.dart';
+import 'package:algo_test/utils/dio_error_extension.dart';
+import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'home_state.dart';
 
 class HomeBloc extends Cubit<HomeState> {
-  HomeBloc() : super(HomeInitial());
+  final HomeRepo _homeRepo;
+  final String underlyingValue;
+
+  HomeBloc({
+    required HomeRepo homeRepo,
+    this.underlyingValue = 'BANKNIFTY',
+  })  : _homeRepo = homeRepo,
+        super(HomeInitial());
+
+  /// [getOptionChainsWithLtp] fetches option-chain
+  /// with TLP for specified underlying value
+  Future<void> getOptionChainsWithLtp() async {
+    try {
+      emit(HomeLoading());
+
+      final response = await _homeRepo.getOptionChains(
+        underlyingValue: underlyingValue,
+      );
+
+      emit(response.options.isNotEmpty
+          ? HomeLoaded(data: response)
+          : HomeEmpty());
+    } on DioException catch (error) {
+      emit(HomeError(error.errorMessage()));
+    }
+  }
 }
