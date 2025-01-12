5cfa2ed62c9a471c3ee34fe2c18dafc126318006
kaxp <kapilsahu232@gmail.com>
2025-01-12 22:12:38 +0530

    Refactor home module binding

--- lib/modules/home/home_module.dart
+++ lib/modules/home/home_module.dart
@@ -2,24 +2,29 @@ import 'package:algo_test/modules/app/base_app_module.dart';
 import 'package:algo_test/modules/home/blocs/home_bloc.dart';
 import 'package:algo_test/modules/home/pages/home_page.dart';
 import 'package:algo_test/modules/home/repositories/home_repo.dart';
+import 'package:algo_test/networking/retrofit/home_api_client.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class HomeModule extends BaseAppModule {
   @override
   List<Bind> get binds => [
-        Bind<HomeRepo>((_) => HomeRepo()),
-        Bind<HomeBloc>((_) => HomeBloc()),
+        Bind.lazySingleton<HomeRepo>(
+          (i) => HomeRepo(homeApiClient: HomeApiClient.withDefaultDio()),
+        ),
+        Bind.factory<HomeBloc>(
+          (i) => HomeBloc(homeRepo: i.get<HomeRepo>()),
+        ),
       ];
 
   @override
   List<ModularRoute> get routes => [
         ChildRoute(
-          Modular.initialRoute,
+          HomeRoute.homePageRoute,
           child: (context, args) => const HomePage(),
         ),
       ];
 }
 
 class HomeRoute {
-  static const String moduleRoute = '/';
+  static const String homePageRoute = '/';
 }
