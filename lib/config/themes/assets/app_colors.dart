a47d82b6a5df4253750453dc207a3886351d4a45
kaxp <kapilsahu232@gmail.com>
2025-01-12 14:20:48 +0530

    Add colours

--- lib/config/themes/assets/app_colors.dart
+++ lib/config/themes/assets/app_colors.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
 mixin AppColors {
+  static const Color primaryColor = Colors.black;
+  static const Color secondaryColor = Color(0xff121319);
+  static const Color backgroundColor = Color(0xff323641);
+  static const Color dividerColour = Color.fromARGB(255, 67, 68, 78);
   static const Color white = Color(0xffffffff);
+  static const Color redColor = Color(0xffF04366);
+  static const Color greenColor = Color(0xff0EBA76);
+  static const Color yellowColor = Color(0xffCDA854);
 }
