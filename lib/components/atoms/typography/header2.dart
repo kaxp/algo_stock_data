36f08a5638ce6e8ff51361285b1b22c9233bc4fd
kaxp <kapilsahu232@gmail.com>
2025-01-12 15:00:27 +0530

    Add reusable text typography

---
+++ lib/components/atoms/typography/header2.dart
@@ -0,0 +1,42 @@
+import 'package:algo_test/config/themes/assets/app_colors.dart';
+import 'package:algo_test/config/themes/assets/app_fonts.dart';
+import 'package:flutter/material.dart';
+
+class Header2 extends StatelessWidget {
+  const Header2({
+    required this.title,
+    this.color,
+    this.height,
+    this.maxLines,
+    this.fontWeight = FontWeight.w500,
+    this.fontSize = 16,
+    this.textAlign = TextAlign.start,
+    this.textOverflow,
+  });
+
+  final String title;
+  final Color? color;
+  final double? height;
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final FontWeight? fontWeight;
+  final double? fontSize;
+  final TextOverflow? textOverflow;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      textAlign: textAlign,
+      maxLines: maxLines,
+      overflow: textOverflow,
+      style: Theme.of(context).textTheme.displayMedium?.copyWith(
+            color: color ?? AppColors.white,
+            height: height,
+            fontWeight: fontWeight,
+            fontFamily: AppFonts.poppins,
+            fontSize: fontSize,
+          ),
+    );
+  }
+}
