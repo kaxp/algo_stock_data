42b8c2c1dd26f53855e4e7e9d473c467b6a6cbf9
kaxp <kapilsahu232@gmail.com>
2025-01-12 14:26:50 +0530

    Add grey text colour to app colours

--- lib/config/themes/assets/app_colors.dart
+++ lib/config/themes/assets/app_colors.dart
@@ -9,4 +9,5 @@ mixin AppColors {
   static const Color redColor = Color(0xffF04366);
   static const Color greenColor = Color(0xff0EBA76);
   static const Color yellowColor = Color(0xffCDA854);
+  static const Color greyTextColor = Color(0xffA0A3AC);
 }
