f555c72da0df026cc4771efe0eaac54f3fbded1c
kaxp <kapilsahu232@gmail.com>
2025-01-13 14:30:57 +0530

    Add empty state widget

---
+++ lib/components/molecules/states/empty_state_view.dart
@@ -0,0 +1,30 @@
+import 'package:algo_test/components/atoms/typography/header2.dart';
+import 'package:algo_test/config/themes/assets/app_colors.dart';
+import 'package:algo_test/config/themes/assets/app_images.dart';
+import 'package:algo_test/constants/app_strings.dart';
+import 'package:algo_test/constants/spacing_constants.dart';
+import 'package:flutter/material.dart';
+
+class EmptyStateView extends StatelessWidget {
+  const EmptyStateView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          AppImages.icDataNotFound(),
+          const SizedBox(
+            height: kSpacingSmall,
+          ),
+          Header2(
+            textAlign: TextAlign.center,
+            title: AppStrings.noResultFound,
+            color: AppColors.blackColor,
+          ),
+        ],
+      ),
+    );
+  }
+}
