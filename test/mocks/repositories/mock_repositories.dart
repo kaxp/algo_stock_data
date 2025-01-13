20db7d71ea99f53ea0b819a417e0c5c222dfbfc9
kaxp <kapilsahu232@gmail.com>
2025-01-13 17:40:22 +0530

    Add mock repositories

---
+++ test/mocks/repositories/mock_repositories.dart
@@ -0,0 +1,9 @@
+import 'package:algo_test/modules/home/repositories/home_repo.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateNiceMocks(
+  [
+    MockSpec<HomeRepo>(),
+  ],
+)
+void main() {}
