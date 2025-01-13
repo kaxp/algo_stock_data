f92c5c3b442114de60f3b386cb9a6ee88e5fbec9
kaxp <kapilsahu232@gmail.com>
2025-01-13 14:30:00 +0530

    Add asset images

--- lib/config/themes/assets/app_images.dart
+++ lib/config/themes/assets/app_images.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 mixin AppImages {
   static const String _IMAGES_FOLDER_PATH = 'assets/images/';
   static const _appLogo = _IMAGES_FOLDER_PATH + 'logo.png';
+  static const String _icDataNotFound =
+      _IMAGES_FOLDER_PATH + 'il_pfa_activity_data_bank_not_found.svg';
 
   static Image icAppLogo({required double height, required double widget}) =>
       Image(
@@ -12,4 +15,6 @@ mixin AppImages {
           _appLogo,
         ),
       );
+
+  static SvgPicture icDataNotFound() => SvgPicture.asset(_icDataNotFound);
 }
