04482f606a81c86ff4332ec19ad6ab45142ad123
kaxp <kapilsahu232@gmail.com>
2025-01-13 16:15:46 +0530

    Add OptionChainListView component

---
+++ lib/components/organisms/list_views/option_chain_list_view.dart
@@ -0,0 +1,38 @@
+import 'package:algo_test/components/molecules/list_items/option_chain_list_item.dart';
+import 'package:algo_test/config/themes/assets/app_colors.dart';
+import 'package:algo_test/modules/home/models/option_chain_response.dart';
+import 'package:flutter/material.dart';
+
+class OptionChainListView extends StatelessWidget {
+  const OptionChainListView({
+    super.key,
+    required this.optionsData,
+    required this.currentExpiryDate,
+  });
+
+  final OptionChainResponse optionsData;
+  final String currentExpiryDate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.separated(
+        shrinkWrap: true,
+        itemCount: optionsData.options[currentExpiryDate]?.strike.length ?? 0,
+        itemBuilder: (context, index) {
+          final strikeRow = optionsData.options[currentExpiryDate];
+
+          return OptionChainListItem(
+            optionData: strikeRow,
+            keyValue: strikeRow?.strike[index],
+            index: index,
+          );
+        },
+        separatorBuilder: (context, index) => const Divider(
+          color: AppColors.dividerColour,
+          height: 1,
+        ),
+      ),
+    );
+  }
+}
