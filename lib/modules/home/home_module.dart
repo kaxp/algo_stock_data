b847e8c5585488cfc370629eea3924f6362bda41
kaxp <kapilsahu232@gmail.com>
2025-01-14 08:15:48 +0530

    Bind WebSocketClient in home module

--- lib/modules/home/home_module.dart
+++ lib/modules/home/home_module.dart
@@ -1,15 +1,28 @@
+import 'dart:convert';
+
 import 'package:algo_test/modules/app/base_app_module.dart';
 import 'package:algo_test/modules/home/blocs/home_bloc.dart';
+import 'package:algo_test/modules/home/models/options_websocket_response.dart';
 import 'package:algo_test/modules/home/pages/home_page.dart';
 import 'package:algo_test/modules/home/repositories/home_repo.dart';
 import 'package:algo_test/networking/retrofit/home_api_client.dart';
+import 'package:algo_test/networking/web_socket_client.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class HomeModule extends BaseAppModule {
   @override
   List<Bind> get binds => [
+        Bind.lazySingleton(
+          (i) => WebSocketClient<OptionsWebSocketResponse>(
+            fromJson: (jsonString) =>
+                OptionsWebSocketResponse.fromJson(jsonDecode(jsonString)),
+          ),
+        ),
         Bind.lazySingleton<HomeRepo>(
-          (i) => HomeRepo(homeApiClient: HomeApiClient.withDefaultDio()),
+          (i) => HomeRepo(
+            homeApiClient: HomeApiClient.withDefaultDio(),
+            webSocketClient: i(),
+          ),
         ),
         Bind.factory<HomeBloc>(
           (i) => HomeBloc(homeRepo: i.get<HomeRepo>()),
