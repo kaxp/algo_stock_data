e454f2c39187d4654c0feda21ef103fe24ed9a78
Kapil Sahu <kapilsahu232@gmail.com>
2025-01-15 10:21:29 +0530

    feature: add units tests (#30)

--- test/modules/home/blocs/home_bloc_test.dart
+++ test/modules/home/blocs/home_bloc_test.dart
@@ -1,17 +1,59 @@
+// ignore_for_file: unused_element
+
 import 'package:algo_test/config/flavor_config.dart';
 import 'package:algo_test/modules/home/blocs/home_bloc.dart';
 import 'package:algo_test/modules/home/home_module.dart';
+import 'package:algo_test/modules/home/models/contracts_response.dart';
 import 'package:algo_test/modules/home/repositories/home_repo.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:algo_test/modules/home/models/option_chain_response.dart';
-import 'package:dio/dio.dart';
 import 'package:modular_test/modular_test.dart';
 
 import '../../../mocks/bloc/mock_blocs.mocks.dart';
+import '../../../mocks/models/test_models_builder.dart';
 import '../../../mocks/repositories/mock_repositories.mocks.dart';
+import '../../../test_utils/mock_dio_exception.dart';
 
+ContractsResponse _defaultContractsResponse() {
+  return buildContractsResponseFromTemplate(
+    contracts: const ContractsData(
+      contractOptions: {
+        "2025-02-27": [
+          ContractOptionData(
+            token: 'NSE_3677',
+            expiry: '2025-02-27',
+            strike: 42000.0,
+            optionType: 'PE',
+          )
+        ]
+      },
+    ),
+  );
+}
+
+OptionChainResponse _defaultOptionChainResponse() {
+  return buildOptionChainResponseFromTemplate(
+    candle: '',
+    underlying: '',
+    impliedFutures: {},
+    options: {
+      '2025-01-01': OptionData(options: [Option(strike: 10000.0)]),
+    },
+  );
+}
+
+OptionChainResponse _defaultOptionChainEmptyResponse() {
+  return buildOptionChainResponseFromTemplate(
+    candle: '',
+    underlying: '',
+    impliedFutures: {},
+    options: {},
+  );
+}
+
+// TODO(kapil): Write remaining unit tests
 void main() {
   late MockHomeRepo _mockHomeRepo;
   late MockHomeBloc _mockHomeBloc;
@@ -35,113 +77,49 @@ void main() {
   });
 
   group('HomeBloc Tests', () {
-    // test('''Given getValidContracts() is called
-    //     When valid contracts are returned
-    //     Then the state should be HomeLoaded''', () async {
-    //   final contractsResponse = ContractsResponse(
-    //     contracts: Contracts(
-    //       contractOptions: [ContractOption(symbol: 'BANKNIFTY')],
-    //     ),
-    //     tokenCache: {
-    //       'BANKNIFTY': ContractOptionData(strike: 10000.0, optionType: 'CE')
-    //     },
-    //   );
-    //   when(_mockHomeRepo.getContracts(underlyingValue: 'BANKNIFTY'))
-    //       .thenAnswer((_) async => contractsResponse);
-
-    //   await _mockHomeBloc.getValidContracts();
-
-    //   expect(_homeBloc.state, isA<HomeLoaded>());
-    // });
-
-    test('''Given getValidContracts() is called
-        When an error occurs in fetching contracts
-        Then the state should be HomeError''', () async {
-      when(_mockHomeRepo.getContracts(underlyingValue: 'BANKNIFTY'))
-          .thenThrow(DioException(
-        requestOptions: RequestOptions(path: ''),
-      ));
-
-      await _mockHomeBloc.getValidContracts();
-
-      expect(_mockHomeBloc.state, isA<HomeError>());
-    });
-
     test('''Given getOptionChainsWithLtp() is called
         When option chain data is successfully returned
         Then the state should be HomeLoaded''', () async {
-      final optionChainResponse = OptionChainResponse(
-          options: {
-            '2025-01-01': OptionData(options: [Option(strike: 10000.0)]),
-          },
-          candle: "",
-          underlying: "BANKNIFTY",
-          impliedFutures: {});
+      final bloc = HomeBloc(homeRepo: _mockHomeRepo);
+
+      bloc.validTokenCache = {};
+
       when(_mockHomeRepo.getOptionChains(underlyingValue: 'BANKNIFTY'))
-          .thenAnswer((_) async => optionChainResponse);
+          .thenAnswer((_) async => _defaultOptionChainResponse());
 
-      await _mockHomeBloc.getOptionChainsWithLtp();
+      await bloc.getOptionChainsWithLtp();
 
-      expect(_mockHomeBloc.state, isA<HomeLoaded>());
+      expect(bloc.state, isA<HomeLoaded>());
     });
 
     test('''Given getOptionChainsWithLtp() is called
-        When an error occurs in fetching option chains
-        Then the state should be HomeError''', () async {
+        When option chain data is empty
+        Then the state should be HomeEmpty''', () async {
+      final bloc = HomeBloc(homeRepo: _mockHomeRepo);
+
+      bloc.validTokenCache = {};
+
       when(_mockHomeRepo.getOptionChains(underlyingValue: 'BANKNIFTY'))
-          .thenThrow(DioException(
-        requestOptions: RequestOptions(path: ''),
-      ));
+          .thenAnswer((_) async => _defaultOptionChainEmptyResponse());
 
-      await _mockHomeBloc.getOptionChainsWithLtp();
+      await bloc.getOptionChainsWithLtp();
 
-      expect(_mockHomeBloc.state, isA<HomeError>());
+      expect(bloc.state, isA<HomeEmpty>());
     });
 
-    // test('''Given connectToOptionsWebSocket() is called
-    //     When a successful WebSocket connection is made
-    //     Then the state should be HomeLoaded''', () async {
-    //   final socketMessage = {
-    //     "msg": {
-    //       "type": "subscribe",
-    //       "datatypes": ["ltp"],
-    //       "underlyings": [
-    //         {
-    //           "underlying": "BANKNIFTY",
-    //           "cash": true,
-    //           "options": ['2025-01-01']
-    //         }
-    //       ]
-    //     }
-    //   };
+    test('''Given getOptionChainsWithLtp() is called
+        When getOptionChainsWithLtp method throw DioException
+        Then the state should be HomeError''', () async {
+      final bloc = HomeBloc(homeRepo: _mockHomeRepo);
 
-    //   when(_mockHomeRepo.connectToOptionsWebSocket()).thenAnswer((_) async {});
-    //   when(_mockHomeRepo.webSocketMessages).thenAnswer((_) => Stream.value(
-    //       WebSocketMessage(ltp: [LtpData(token: 'BANKNIFTY', ltp: 100.0)])));
+      bloc.validTokenCache = {};
 
-    //   await _mockHomeBloc.connectToOptionsWebSocket();
+      when(_mockHomeRepo.getOptionChains(underlyingValue: 'BANKNIFTY'))
+          .thenAnswer((_) async => throw MockDioException());
 
-    //   expect(_mockHomeBloc.state, isA<HomeLoaded>());
-    // });
+      await bloc.getOptionChainsWithLtp();
 
-    test('''Given connectToOptionsWebSocket() is called
-        When a WebSocket connection fails
-        Then the state should be HomeOptionsSocketError''', () async {
-      when(_mockHomeRepo.connectToOptionsWebSocket())
-          .thenThrow(DioException(requestOptions: RequestOptions(path: '')));
-
-      await _mockHomeBloc.connectToOptionsWebSocket();
-
-      expect(_mockHomeBloc.state, isA<HomeOptionsSocketError>());
+      expect(bloc.state, isA<HomeError>());
     });
-
-    // test('''Given onFilterChange() is called
-    //     When a new expiry date is selected
-    //     Then the state should update with the new expiry date''', () async {
-    //   await _mockHomeBloc.onFilterChange('2025-02-01');
-
-    //   expect(_mockHomeBloc.state, isA<HomeLoaded>());
-    //   expect((_mockHomeBloc.state as HomeLoaded).currentExpiryDate, '2025-02-01');
-    // });
   });
 }
